d8fd942399ba9dc9cb232cb9c72b6afabc56d764
Jmendes <jmendes.3595@gmail.com>
2024-05-12 15:27:21 +0100

    px-5 set up project

---
+++ tests/PuppetXAppTests.cpp
@@ -0,0 +1,14 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(test, testing)
+{
+    ASSERT_TRUE(true);
+}
